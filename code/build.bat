c6fceb33d26c4047e7daed23bccdcedd5073a418
Ihor Szlachtycz <ihor.szlachtycz@amd.com>
2020-12-15 16:03:59 -0500

    Adding ignore

--- code/build.bat
+++ code/build.bat
@@ -2,7 +2,7 @@
 
 set CodeDir=..\code
 set DataDir=..\data
-set LibsDir=D:\Code\Libs
+set LibsDir=..\libs
 set OutputDir=..\build_win32
 set VulkanIncludeDir="C:\VulkanSDK\1.2.135.0\Include\vulkan"
 set VulkanBinDir="C:\VulkanSDK\1.2.135.0\Bin"
@@ -22,20 +22,20 @@ pushd %OutputDir%
 del *.pdb > NUL 2> NUL
 
 REM USING GLSL IN VK USING GLSLANGVALIDATOR
-REM call glslangValidator -DGBUFFER_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_gbuffer_vert.spv %CodeDir%\deferred_shaders.cpp
-REM call glslangValidator -DGBUFFER_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_gbuffer_frag.spv %CodeDir%\deferred_shaders.cpp
-REM call glslangValidator -DPOINT_LIGHT_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_point_light_vert.spv %CodeDir%\deferred_shaders.cpp
-REM call glslangValidator -DPOINT_LIGHT_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_point_light_frag.spv %CodeDir%\deferred_shaders.cpp
-REM call glslangValidator -DDIRECTIONAL_LIGHT_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_directional_light_vert.spv %CodeDir%\deferred_shaders.cpp
-REM call glslangValidator -DDIRECTIONAL_LIGHT_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_directional_light_frag.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DGBUFFER_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_gbuffer_vert.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DGBUFFER_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_gbuffer_frag.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DPOINT_LIGHT_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_point_light_vert.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DPOINT_LIGHT_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_point_light_frag.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DDIRECTIONAL_LIGHT_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_deferred_directional_light_vert.spv %CodeDir%\deferred_shaders.cpp
+call glslangValidator -DDIRECTIONAL_LIGHT_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_deferred_directional_light_frag.spv %CodeDir%\deferred_shaders.cpp
 
-REM call glslangValidator -DVERTEX_SHADER=1 -S vert -e main -g -V -o %DataDir%\shader_forward_vert.spv %CodeDir%\shader_forward.cpp
-REM call glslangValidator -DFRAGMENT_SHADER=1 -S frag -e main -g -V -o %DataDir%\shader_forward_frag.spv %CodeDir%\shader_forward.cpp
+call glslangValidator -DVERTEX_SHADER=1 -S vert -e main -g -V -o %DataDir%\shader_forward_vert.spv %CodeDir%\shader_forward.cpp
+call glslangValidator -DFRAGMENT_SHADER=1 -S frag -e main -g -V -o %DataDir%\shader_forward_frag.spv %CodeDir%\shader_forward.cpp
 
-REM call glslangValidator -DGRID_FRUSTUM=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_forward_grid_frustum.spv %CodeDir%\tiled_forward_shaders.cpp
-REM call glslangValidator -DLIGHT_CULLING=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_forward_light_culling.spv %CodeDir%\tiled_forward_shaders.cpp
-REM call glslangValidator -DTILED_FORWARD_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_tiled_forward_vert.spv %CodeDir%\tiled_forward_shaders.cpp
-REM call glslangValidator -DTILED_FORWARD_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_tiled_forward_frag.spv %CodeDir%\tiled_forward_shaders.cpp
+call glslangValidator -DGRID_FRUSTUM=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_forward_grid_frustum.spv %CodeDir%\tiled_forward_shaders.cpp
+call glslangValidator -DLIGHT_CULLING=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_forward_light_culling.spv %CodeDir%\tiled_forward_shaders.cpp
+call glslangValidator -DTILED_FORWARD_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_tiled_forward_vert.spv %CodeDir%\tiled_forward_shaders.cpp
+call glslangValidator -DTILED_FORWARD_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_tiled_forward_frag.spv %CodeDir%\tiled_forward_shaders.cpp
 
 call glslangValidator -DGRID_FRUSTUM=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_deferred_grid_frustum.spv %CodeDir%\tiled_deferred_shaders.cpp
 call glslangValidator -DLIGHT_CULLING=1 -S comp -e main -g -V -o %DataDir%\shader_tiled_deferred_light_culling.spv %CodeDir%\tiled_deferred_shaders.cpp
@@ -44,7 +44,7 @@ call glslangValidator -DGBUFFER_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader
 call glslangValidator -DTILED_DEFERRED_LIGHTING_VERT=1 -S vert -e main -g -V -o %DataDir%\shader_tiled_deferred_lighting_vert.spv %CodeDir%\tiled_deferred_shaders.cpp
 call glslangValidator -DTILED_DEFERRED_LIGHTING_FRAG=1 -S frag -e main -g -V -o %DataDir%\shader_tiled_deferred_lighting_frag.spv %CodeDir%\tiled_deferred_shaders.cpp
 
-REM call glslangValidator -DFRAGMENT_SHADER=1 -S frag -e main -g -V -o %DataDir%\shader_copy_to_swap_frag.spv %CodeDir%\shader_copy_to_swap.cpp
+call glslangValidator -DFRAGMENT_SHADER=1 -S frag -e main -g -V -o %DataDir%\shader_copy_to_swap_frag.spv %CodeDir%\shader_copy_to_swap.cpp
 
 REM USING HLSL IN VK USING DXC
 REM set DxcDir=C:\Tools\DirectXShaderCompiler\build\Debug\bin
